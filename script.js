4ab6f6d0b382ee78999795c0a68f54d21a52c1b7
Hey-SE <heysese441@gmail.com>
2024-09-01 23:35:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openForm(url) {
+    window.open(url, '_blank');
+}
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
